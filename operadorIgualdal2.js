7ffbf5510616bafabf49f9cdcdaf57d72771b5dd
rafael1995coronado <edwim19952017@gmail.com>
2017-05-13 11:22:00 -0400

    rafa

---
+++ operadorIgualdal2.js
@@ -0,0 +1,12 @@
+console.log("x;");
+console.log(+"15" +5);
+console.log("88" +1);
+console.log(typeof ("88+1"));
+console.log(-"5"+15);
+console.log(-5-"5");
+console.log("f:");
+var x=1;
+var y=88;
+var z=0;
+z=++y+z;
+console.log(z);
---
+++ tablaSuma.js
@@ -0,0 +1,4 @@
+var x=7;
+var y=0;
+while (y<10){console.log(x + " + " + ++y +"="+(x+y));}
+
